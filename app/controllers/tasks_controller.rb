1acd4a13a00a0680c281ba5d1c054216a7dae320
ehiro276 <69147300+ehiro276@users.noreply.github.com>
2020-09-22 08:59:43 +0900

    タスク作成画面への移動

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,5 +2,18 @@ class TasksController < ApplicationController
   def index
   end
 
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
   
 end
