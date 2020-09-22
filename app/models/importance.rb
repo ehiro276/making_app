8602b7a44ef8533b59db76ba78b2b1331d55a200
ehiro276 <69147300+ehiro276@users.noreply.github.com>
2020-09-22 11:39:51 +0900

    重要性の入力

--- app/models/importance.rb
+++ app/models/importance.rb
@@ -1,8 +1,8 @@
-class Genre < ActiveHash::Base
+class Importance < ActiveHash::Base
   self.data = [
     { id: 1, name: '--' },
-    { id: 2, name: '1' },
-    { id: 3, name: '2' },
-    { id: 4, name: '3' },
+    { id: 2, name: '1日前' },
+    { id: 3, name: '2日前' },
+    { id: 4, name: '3日前' },
   ]
 end
